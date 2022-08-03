776e2e28dc7da1a3f48be27651523fa091b1d3b7
satish <satishsunny1550@gmail.com>
2022-08-03 17:30:04 +0530

    App

--- src/App.js
+++ src/App.js
@@ -8,42 +8,17 @@ function App() {
     return (
       <Cards 
         key= {items.id}
-        title= {items.title}
-        imageUrl= {items.imageUrl}
-        location= {items.location}
-        description= {items.description}
-        googleMapsUrl= {items.googleMapsUrl}
-        startDate= {items.startDate}
+        {...items}
       />
     )
   })
   return (
     <div className='container'>
       <Header />
-      {/* <img src= {logo} alt="logo" /> */}
-      {/* <img src="../images/MountFuji.jpg" alt="" /> */}
       <section>
         {Card}
       </section>
     </div>
-
-    // <h1 className="App-header">satish</h1>
-    // <div className="App">
-    //   <header >
-    //     {/* <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header> */}
-    // </div>
   );
 }
 
--- src/components/cards.js
+++ src/components/cards.js
@@ -2,16 +2,20 @@
 export default function Cards(porps) {
     return (
         <div>
-            <img src={`../images/${porps.imageUrl}`} alt="images" className="images"/>
-
-            {/* <span className="images"></span> */}
-            {/* <div style={{
-                backgroundImage: `url("https://www.pinterest.com/pin/759067712188649015/")`
-            }}>
-            </div> */}
+            <img  
+                src={require(`../images/${porps.imageUrl}`)} 
+                alt="images" 
+                className="images"
+            />
             <p className="location">{porps.location}</p>
-            <p><a className="googleMaps" href='${{porps.googleMapsUrl}}'>  View on Google Maps</a></p>
-            <h3 className="title">{porps.title}</h3>
+            <p>
+                <a 
+                    href={`${porps.googleMapsUrl}`}
+                    className="googleMaps" 
+                    >View on Google Maps
+                </a>
+            </p>
+                <h3 className="title">{porps.title}</h3>
             <p>
                 <h4 className="stardate">{porps.startDate} </h4>
             </p>
